9d85bd8ea91d2a5155bc61d0c8f9cbcbbb505100
arina <aricrate@gmail.com>
2025-05-14 21:47:34 +0300

    change db con

--- php/db.php
+++ php/db.php
@@ -1,11 +1,12 @@
 <?php
 $user='root';
-$passward='root';
+$passward='1234';
 $dbname='hostel';
 $host='localhost';
+$port='3306';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
